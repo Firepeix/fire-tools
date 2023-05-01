c6a9fde8b61a43728de4d7fb2006e4b0acf52e61
Arthur Fernandes <arthurhmf@gmail.com>
2023-05-01 15:08:20 -0300

    feat: adiciona metodos de adicionar usuarios

--- mysql/devenv.nix
+++ mysql/devenv.nix
@@ -7,12 +7,26 @@
 
 
   services.mysql.enable = true; 
+	
+  services.mysql.ensureUsers = [
+    {
+      name = "tutu";
+      password = "secret";
+      ensurePermissions = { "*.*" = "ALL PRIVILEGES"; };
+    }
+  ];
+
+  services.mysql.settings.mysqld = {
+    "skip_name_resolve" = "on";
+  }; 
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo Iniciando MySql";
+#  scripts.addUser.exec = "mysql -u root -e \"CREATE USER 'tutu'@'192.168.0.11' IDENTIFIED BY 'secret';GRANT ALL PRIVILEGES ON *.* TO 'tutu'@'192.168.0.11';\"";
 
   enterShell = ''
     hello
+#    addUser
   '';
 
   # https://devenv.sh/languages/
